1925b9f12e28b9a27469c53e212df7f2f3984deb
Jacob <jahammer@uark.edu>
2015-07-29 03:58:00 -0500

    haxe make
    READ TITLE FUCK

---
+++ New Project/src/Main.hx
@@ -0,0 +1,22 @@
+package;
+
+import openfl.display.Sprite;
+import openfl.Lib;
+
+/**
+ * ...
+ * @author Jacob
+ * @author Tara
+ */
+class Main extends Sprite 
+{
+
+	public function new() 
+	{
+		super();
+		
+		// Assets:
+		// openfl.Assets.getBitmapData("img/assetname.jpg");
+	}
+
+}
